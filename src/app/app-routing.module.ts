94a34118f4c752e1360a60459a213efae8fd5ff6
asari <32879431+deomorxsy@users.noreply.github.com>
2023-05-03 23:03:51 -0300

    add basic routing syntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "/home",
+    component: AppComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    redirectTo: "home"
+  },
+  {
+    path: "/crud/add",
+    component: AppComponent,
+    redirectTo: "home"
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
